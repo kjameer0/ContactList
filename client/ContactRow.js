08c644692280ad239c3c3a73529ffb303e5d38dc
Rachel Or <rachelor7@gmail.com>
2022-03-17 11:24:38 -0500

    table looks good

---
+++ client/ContactRow.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const ContactRow = (props) => {
+  console.log(props);
+  return (
+    <tr>
+      <td>{props.contact.name}</td>
+      <td>{props.contact.phone}</td>
+      <td>{props.contact.email}</td>
+    </tr>
+  );
+};
+
+export default ContactRow;
